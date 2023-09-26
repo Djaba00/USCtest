8abb8f3a724af2c3f9f6bd73b137172788ddea4c
DJaba00 <shipelenko9@gmail.com>
2023-09-26 16:37:27 +0500

    рефакторинг тестового проекта

--- USCtest.BLLTests/Services/TaxServiceTests.cs
+++ USCtest.BLLTests/Services/TaxServiceTests.cs
@@ -16,18 +16,6 @@ namespace USCtest.BLL.Services.Tests
         [TestMethod()]
         public void CalculateTest()
         {
-            var flat = new FlatModel()
-            {
-                Street = "Pushkina",
-                StreetNumber = 10,
-                FlatNumber = 5,
-                IsColdWatherDevice = true,
-                IsElectricPowerDevice = true,
-                IsHotWatherDevice = true,
-                Users = new List<UserModel>(),
-                Taxes = new List<TaxModel>()
-            };
-
             var indications = new FlatIndications()
             {
                 ColdWather = 100,
@@ -37,34 +25,48 @@ namespace USCtest.BLL.Services.Tests
                 ElectricityNight = 100
             };
 
+            var flat = new FlatModel()
+            {
+                Street = "Pushkina",
+                StreetNumber = 10,
+                FlatNumber = 5,
+                IsColdWatherDevice = true,
+                IsElectricPowerDevice = true,
+                IsHotWatherDevice = true,
+                Users = new List<UserModel>(),
+                Taxes = new List<TaxModel>(),
+                Indications = indications,
+            };
+
+            var userFlats = new List<FlatModel>() { flat };
+
             var user = new UserModel()
             {
                 FirstName = "Test",
                 LastName = "Testovich",
                 PassportSeries = "1010",
                 PassportNumber = "101010",
-                Flat = flat,
-                Indications = indications,
+                Flats = userFlats,
             };
 
             var taxService = new TaxService(null, null);
 
-            taxService.CalculateForTest(user);
+            taxService.CalculateForTest(flat);
 
-            Assert.AreEqual(user.Flat.Taxes.First().ColdWaterVolume, 100);
-            Assert.AreEqual(user.Flat.Taxes.First().ColdWatherCost, 3578);
+            Assert.AreEqual(flat.Taxes.First().ColdWaterVolume, 100);
+            Assert.AreEqual(flat.Taxes.First().ColdWatherCost, 3578);
 
-            Assert.AreEqual(user.Flat.Taxes.First().HotWatherHeatVolume, 100);
-            Assert.AreEqual(user.Flat.Taxes.First().HotWatherHeatCost, 3578);
+            Assert.AreEqual(flat.Taxes.First().HotWatherHeatVolume, 100);
+            Assert.AreEqual(flat.Taxes.First().HotWatherHeatCost, 3578);
 
-            Assert.AreEqual(user.Flat.Taxes.First().HotWatherThermalEnergyVolume, 5.349);
-            Assert.AreEqual(user.Flat.Taxes.First().HotWatherThermalEnergyCost, 5341.99281m);
+            Assert.AreEqual(flat.Taxes.First().HotWatherThermalEnergyVolume, 5.349);
+            Assert.AreEqual(flat.Taxes.First().HotWatherThermalEnergyCost, 5341.99281m);
 
-            Assert.AreEqual(user.Flat.Taxes.First().ElectricityDayVolume, 100);
-            Assert.AreEqual(user.Flat.Taxes.First().ElectricityDayCost, 490);
+            Assert.AreEqual(flat.Taxes.First().ElectricityDayVolume, 100);
+            Assert.AreEqual(flat.Taxes.First().ElectricityDayCost, 490);
 
-            Assert.AreEqual(user.Flat.Taxes.First().ElectricityNightVolume, 100);
-            Assert.AreEqual(user.Flat.Taxes.First().ElectricityNightCost, 231);
+            Assert.AreEqual(flat.Taxes.First().ElectricityNightVolume, 100);
+            Assert.AreEqual(flat.Taxes.First().ElectricityNightCost, 231);
         }
     }
 }
