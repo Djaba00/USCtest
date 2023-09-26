9f0f3bce56f6ac1dba1565e26e5a5d9b93393570
Djaba00 <shipelenko9@gmail.com>
2023-09-26 15:02:30 +0500

    Удален лишний User service

--- USCtest.BLL/Services/UserService.cs
+++ USCtest.BLL/Services/UserService.cs
@@ -13,152 +13,154 @@ namespace USCtest.BLL.Services
 {
     public class UserService : IUserService
     {
-        IUnitOfWork db;
-        IMapper mapper;
-
-        public UserService(IUnitOfWork db, IMapper mapper)
+        public class UserService : IUserService
         {
-            this.db = db;
-            this.mapper = mapper;
-        }
+            IUnitOfWork db;
+            IMapper mapper;
 
-        public async Task<UserDTO> GetUserById(string id)
-        {
-            var user = await db.UsersManager.FindByIdAsync(id);
-
-            if (user == null)
+            public UserService(IUnitOfWork db, IMapper mapper)
             {
-                return mapper.Map<UserDTO>(user);
+                this.db = db;
+                this.mapper = mapper;
             }
-            else
+
+            public async Task<UserDTO> GetUserById(string id)
             {
-                throw new Exception("Пользователь не найден");
-            }
-        }
+                var user = await db.UsersManager.FindByIdAsync(id);
 
-        public async Task<List<UserDTO>> GetUsersByName(string name)
-        {
-            var users = await db.UsersManager.Users.Where(c => c.GetFullName().Contains(name)).ToListAsync();
-
-            if (users == null)
-            {
-                var usersDto = new List<UserDTO>();
-
-                foreach (var user in users)
+                if (user != null)
                 {
-                    usersDto.Add(mapper.Map<UserDTO>(user));
-                }
-
-                return usersDto;
-            }    
-            else
-            {
-                throw new Exception("Пользователи не найдены");
-            }
-        }
-
-        public async Task CreateUser(UserDTO userDto)
-        {
-            if (userDto != null)
-            {
-                var user = mapper.Map<User>(userDto);
-
-                await db.UsersManager.CreateAsync(user);
-            }
-            else
-            {
-                throw new Exception("Не корректный формат данных пользователя");
-            }
-        }
-
-        public async Task ChangePassword(UserDTO userDto, string currentPassword, string newPassword)
-        {
-            if (userDto != null)
-            {
-                var user = mapper.Map<User>(userDto);
-
-                var currentUser = await db.UsersManager.FindByIdAsync(user.Id);
-
-                if (currentUser != null)
-                {
-                    await db.UsersManager.ChangePasswordAsync(currentUser, currentPassword, newPassword);
+                    return mapper.Map<UserDTO>(user);
                 }
                 else
                 {
                     throw new Exception("Пользователь не найден");
                 }
             }
-            else
-            {
-                throw new Exception("Не корректный формат данных пользователя");
-            }
-        }
 
-        public async Task ChangeRegistration(UserDTO userDto)
-        {
-            if (userDto != null)
+            public async Task<List<UserDTO>> GetUsersByName(string name)
             {
-                var user = mapper.Map<User>(userDto);
-                var currentUser = await db.UsersManager.FindByIdAsync(user.Id);
+                var users = await db.UsersManager.Users.Where(c => c.GetFullName().Contains(name)).ToListAsync();
+
+                if (users != null)
+                {
+                    var usersDto = new List<UserDTO>();
+
+                    foreach (var user in users)
+                    {
+                        usersDto.Add(mapper.Map<UserDTO>(user));
+                    }
+
+                    return usersDto;
+                }
+                else
+                {
+                    throw new Exception("Пользователи не найдены");
+                }
+            }
+
+            public async Task CreateUser(UserDTO userDto)
+            {
+                if (userDto != null)
+                {
+                    var user = mapper.Map<User>(userDto);
+
+                    await db.UsersManager.CreateAsync(user);
+                }
+                else
+                {
+                    throw new Exception("Не корректный формат данных пользователя");
+                }
+            }
+
+            public async Task ChangePassword(UserDTO userDto, string currentPassword, string newPassword)
+            {
+                if (userDto != null)
+                {
+                    var user = mapper.Map<User>(userDto);
+
+                    var currentUser = await db.UsersManager.FindByIdAsync(user.Id);
+
+                    if (currentUser != null)
+                    {
+                        await db.UsersManager.ChangePasswordAsync(currentUser, currentPassword, newPassword);
+                    }
+                    else
+                    {
+                        throw new Exception("Пользователь не найден");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Не корректный формат данных пользователя");
+                }
+            }
+
+            public async Task ChangeRegistration(UserDTO userDto)
+            {
+                if (userDto != null)
+                {
+                    var user = mapper.Map<User>(userDto);
+                    var currentUser = await db.UsersManager.FindByIdAsync(user.Id);
+
+                    if (currentUser != null)
+                    {
+                        currentUser.Flat = user.Flat;
+                        await db.UsersManager.UpdateAsync(currentUser);
+                    }
+                    else
+                    {
+                        throw new Exception("Пользователь не найден");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Не корректный формат данных пользователя");
+                }
+            }
+
+            public async Task UpdateUserAccount(UserDTO userDto)
+            {
+                if (userDto != null)
+                {
+                    var user = mapper.Map<User>(userDto);
+                    var currentUser = await db.UsersManager.FindByIdAsync(user.Id);
+
+                    if (currentUser != null)
+                    {
+                        currentUser.FirstName = user.FirstName;
+                        currentUser.LastName = user.LastName;
+                        currentUser.MiddleName = user.MiddleName;
+                        currentUser.Email = user.Email;
+                        currentUser.PassportSeries = user.PassportSeries;
+                        currentUser.PassportNumber = user.PassportNumber;
+                        currentUser.Flat = currentUser.Flat;
+
+                        await db.UsersManager.UpdateAsync(currentUser);
+                    }
+                    else
+                    {
+                        throw new Exception("Пользователь не найден");
+                    }
+                }
+                else
+                {
+                    throw new Exception("Не корректный формат данных пользователя");
+                }
+            }
+
+            public async Task DeleteUser(string id)
+            {
+                var currentUser = await db.UsersManager.FindByIdAsync(id);
 
                 if (currentUser != null)
                 {
-                    currentUser.Flat = user.Flat;
-                    await db.UsersManager.UpdateAsync(currentUser);
+                    await db.UsersManager.DeleteAsync(currentUser);
                 }
                 else
                 {
                     throw new Exception("Пользователь не найден");
                 }
-            }
-            else
-            {
-                throw new Exception("Не корректный формат данных пользователя");
-            }
-        }
-
-        public async Task UpdateUserAccount(UserDTO userDto)
-        {
-            if (userDto != null)
-            {
-                var user = mapper.Map<User>(userDto);
-                var currentUser = await db.UsersManager.FindByIdAsync(user.Id);
-
-                if (currentUser != null)
-                {
-                    currentUser.FirstName = user.FirstName;
-                    currentUser.LastName = user.LastName;
-                    currentUser.MiddleName = user.MiddleName;
-                    currentUser.Email = user.Email;
-                    currentUser.PassportSeries = user.PassportSeries;
-                    currentUser.PassportNumber = user.PassportNumber;
-                    currentUser.Flat = currentUser.Flat; 
-
-                    await db.UsersManager.UpdateAsync(currentUser);
-                }
-                else
-                {
-                    throw new Exception("Пользователь не найден");
-                }
-            }
-            else
-            {
-                throw new Exception("Не корректный формат данных пользователя");
-            }
-        }
-
-        public async Task DeleteUser(string id)
-        {
-            var currentUser = await db.UsersManager.FindByIdAsync(id);
-
-            if (currentUser != null)
-            {
-                await db.UsersManager.DeleteAsync(currentUser);
-            }
-            else
-            {
-                throw new Exception("Пользователь не найден");
             }
         }
     }
-}
